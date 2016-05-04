61a2241daee06afdb5258ad0b69bec3747a2fbfa
Stephen Sullivan <sullivankstephen@gmail.com>
2016-05-04 13:00:27 -0500

    Moved everything to everywhere jesus

--- contentscript.js
+++
@@ -1,5 +0,0 @@
-console.log("Attempting to inject payload...");
-var s = document.createElement('script');
-s.src = chrome.extension.getURL("payload.js");
-(document.head||document.documentElement).appendChild(s);
-//s.parentNode.removeChild(s);
